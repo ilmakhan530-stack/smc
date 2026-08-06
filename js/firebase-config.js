cb1b11c49cf8a07860f8366aa54c0cee78ff01a4
ilmakhan530-stack <ilmakhan530@gmail.com>
2026-08-07 00:35:27 +0530

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,15 @@
-// Firebase configuration yahan add hogi
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6aTUL9X2hiT86E5HDo8kg6G-Q1m1Hk0M",
+  authDomain: "smc-e8841.firebaseapp.com",
+  projectId: "smc-e8841",
+  storageBucket: "smc-e8841.firebasestorage.app",
+  messagingSenderId: "648745398947",
+  appId: "1:648745398947:web:6f72ee13e54134ddb48516"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
